0bb7167b176aac19aaa2dc0d2b0f89e3dc16e308
CalogeroCantone <calolavendetta@gmail.com>
2022-09-22 18:12:04 +0200

    Fix build problem.

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,6 +4,7 @@ module.exports = {
   title: 'Calogero Cantone',
   description: 'Just playing around.',
   base: "/vitepress-starter/",
+  ignoreDeadLinks: true,
   markdown: {
     config: MermaidPlugin,
   },
