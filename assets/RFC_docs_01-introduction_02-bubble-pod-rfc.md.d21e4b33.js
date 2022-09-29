7bab1e11a269cb2600964aed98d43c51b3c21325
CalogeroCantone <CalogeroCantone@users.noreply.github.com>
2022-09-29 10:26:03 +0000

    deploy: 2af88c9ff8179d8353402c5177031449fe4e0ec7

---
+++ assets/RFC_docs_01-introduction_02-bubble-pod-rfc.md.d21e4b33.js
@@ -0,0 +1,20 @@
+import{_ as s,c as n,o as a,a as e}from"./app.09178a68.js";const C=JSON.parse('{"title":"BubblePod RFC","description":"","frontmatter":{},"headers":[{"level":2,"title":"Summary","slug":"summary","link":"#summary","children":[]},{"level":2,"title":"Content","slug":"content","link":"#content","children":[]}],"relativePath":"RFC/docs/01-introduction/02-bubble-pod-rfc.md","lastUpdated":1664447137000}'),l={name:"RFC/docs/01-introduction/02-bubble-pod-rfc.md"},p=e(`<h1 id="bubblepod-rfc" tabindex="-1">BubblePod RFC <a class="header-anchor" href="#bubblepod-rfc" aria-hidden="true">#</a></h1><p><strong>RFC NUM</strong>: 01-02</p><h2 id="summary" tabindex="-1">Summary <a class="header-anchor" href="#summary" aria-hidden="true">#</a></h2><p>A Request for Comment for BubblePod Request for Comment.</p><h2 id="content" tabindex="-1">Content <a class="header-anchor" href="#content" aria-hidden="true">#</a></h2><p>Template:</p><div class="language-markdown line-numbers-mode"><button class="copy"></button><span class="lang">markdown</span><pre><code><span class="line"><span style="color:#89DDFF;"># </span><span style="color:#FFCB6B;">RFC Title</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#89DDFF;font-style:italic;">__</span><span style="color:#F07178;font-style:italic;">RFC NUM</span><span style="color:#89DDFF;font-style:italic;">__</span><span style="color:#A6ACCD;">: {NUMBER_OF_CHAPTER}-{NUMBER_OF_RFC_OF_CHAPTER}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#89DDFF;">## </span><span style="color:#FFCB6B;">Summary</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">A brief summary of the Request for Comment.</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#89DDFF;">## </span><span style="color:#FFCB6B;">Content</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">The full content of the Request for Comment.</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#89DDFF;">## </span><span style="color:#FFCB6B;">Authors</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#89DDFF;">1.</span><span style="color:#A6ACCD;"> </span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;"> Full Name: </span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;"> Email: </span></span>
+<span class="line"><span style="color:#A6ACCD;">    </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;"> Role: CEO | CTO | CIO | Developer | PM</span></span>
+<span class="line"></span>
+<span class="line"></span></code></pre><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br></div></div>`,7),r=[p];function o(t,c,i,b,m,u){return a(),n("div",null,r)}const F=s(l,[["render",o]]);export{C as __pageData,F as default};
